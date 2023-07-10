b1851253313bff31aa8ef8c158bea90f50ad809c
JhoniWu <1291063373@qq.com>
2023-07-10 11:52:27 +0800

    Initial commit

---
+++ src/com/minis/BeanDefinition.java
@@ -0,0 +1,32 @@
+package com.minis;
+
+/**
+ * @program: MicroSpring
+ * @description:
+ * @author: Max Wu
+ * @create: 2023-07-10 11:05
+ **/
+public class BeanDefinition {
+	private String id;
+	private String className;
+	public BeanDefinition (String id, String className){
+		this.id = id;
+		this.className = className;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+}
